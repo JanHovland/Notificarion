f117518f00519819a38929c4ee1f1123c3301b13
Jan Hovland <jan.hovland@lyse.net>
2019-04-05 20:06:48 +0200

    Update

--- Notificarion/ViewController.swift
+++ Notificarion/ViewController.swift
@@ -7,14 +7,211 @@
 //
 
 import UIKit
+import UserNotifications
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 1. Configuring the notification content
+        let content = UNMutableNotificationContent()
+        content.title = "Weekly Staff Meeting"
+        content.body = "Every Tuesday at 2pm"
+        
+        // 2. Configuring a recurring date-based trigger
+        // Configure the recurring date.
+        var dateComponents = DateComponents()
+        dateComponents.calendar = Calendar.current
+        
+        dateComponents.weekday = 6  // Tuesday
+        dateComponents.hour = 20    // 19:00 hours
+        dateComponents.minute = 05 // 19:45 hours
+        // Create the trigger as a repeating event.
+        let trigger = UNCalendarNotificationTrigger(
+            dateMatching: dateComponents, repeats: true)
+
+        // 3. Registering the notification request
+        // Create the request
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidString,
+                                            content: content, trigger: trigger)
+        
+        // Schedule the request with the system.
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.add(request) { (error) in
+            if error != nil {
+                // Handle any errors.
+            }
+            
+        }
+
+        // Avslutter appen
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            UIApplication.shared.perform(#selector(NSXPCConnection.suspend))
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                exit(0)
+            }
+        }
+        
+    }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+import UIKit
+
+
+let lightNotificationKey = "co.seanallen.lightSide"
+let darkNotificationKey = "co.seanallen.darkSide"
+
+
+class SelectionScreen: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func imperialButtonTapped(_ sender: UIButton) {
+        let name = Notification.Name(rawValue: darkNotificationKey)
+        NotificationCenter.default.post(name: name, object: nil)
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func rebelButtonTapped(_ sender: UIButton) {
+        let name = Notification.Name(rawValue: lightNotificationKey)
+        NotificationCenter.default.post(name: name, object: nil)
+        dismiss(animated: true, completion: nil)
+    } }
+
+import UIKit
+let lightNotificationKey = "co.seanallen.lightSide"
+let darkNotificationKey = "co.seanallen.darkSide"
+
+class BaseScreen: UIViewController {
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var chooseButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    let light = Notification.Name(rawValue: lightNotificationKey)
+    let dark = Notification.Name(rawValue: darkNotificationKey)
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        chooseButton.layer.cornerRadius = chooseButton.frame.size.height/2
+        createObservers()
+    }
+    func createObservers() {
+        //Light
+        NotificationCenter.default.addObserver(self, selector:
+            #selector(BaseScreen.updateCharacterImage(notification:)), name: light, object: nil)
+        NotificationCenter.default.addObserver(self, selector:
+            #selector(BaseScreen.updateNameLabel(notification:)), name: light, object: nil)
+        NotificationCenter.default.addObserver(self, selector:
+            #selector(BaseScreen.updateBackground(notification:)), name: light, object: nil)
+        //Dark
+        NotificationCenter.default.addObserver(self, selector:
+            #selector(BaseScreen.updateCharacterImage(notification:)), name: dark, object: nil)
+        NotificationCenter.default.addObserver(self, selector:
+            #selector(BaseScreen.updateNameLabel(notification:)), name: dark, object: nil)
+        NotificationCenter.default.addObserver(self, selector:
+            #selector(BaseScreen.updateBackground(notification:)), name: dark, object: nil)
+    }
+    @objc func updateCharacterImage(notification: NSNotification) {
+        let isLight = notification.name == light
+        let image = isLight ? UIImage(named: "luke")! : UIImage(named: "vader")!
+        mainImageView.image = image
+    }
+    @objc func updateNameLabel(notification: NSNotification) {
+        let isLight = notification.name == light
+        let name = isLight ? "Luke Skywalker" : "Darth Vader"
+        nameLabel.text = name
+    }
+    @objc func updateBackground(notification: NSNotification) {
+        let isLight = notification.name == light
+        let color = isLight ? UIColor.cyan : UIColor.red
+        view.backgroundColor = color
+    }
+    
+    /*
+    @IBAction func chooseButtonTapped(_ sender: UIButton) {
+        let selectionVC = storyboard?.instantiateViewController(withIdentifier:
+            "SelectionScreen") as! SelectionScreen
+        present(selectionVC, animated: true, completion: nil)
+    }
+    */
+    
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 1. Ask the user for permission
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .sound]) {
+            (granted, error) in
+        }
+        
+        // 2. Create the notification content
+        let content = UNMutableNotificationContent()
+        content.title = "Notification"
+        content.body = "Look at me"
+        
+        // 3. Create the trigger
+        let date = Date().addingTimeInterval(10)
+        
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        // 4. Create the request
+        let uuidString = UUID().uuidString
+        
+        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+        
+        // 5. Register wiyh notification center
+        
+        center.add(request) { (error) in
+            // Check the error parameter and handle any errors
+        }
     }
 
 
 }
 
+*/
